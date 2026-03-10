f08dd2a671f74d2f0334c0099912dd6a75b6c5d9
Fred Dickinson <fredsterdickinson@gmail.com>
2026-03-10 22:56:24 +0000

    Threshold and colour some signatures

--- Notebooks/cleaning.py
+++ Notebooks/cleaning.py
@@ -5,11 +5,12 @@ import numpy as np
 import pandas as pd 
 import re
 
-def clean_data(df, relevant_cols, early_age = 50):
+def clean_data(raw_df, relevant_cols, early_age = 50):
     '''
     Collects all data cleaning functions into one pipeline, returning the clean data.
     '''
 
+    df = raw_df.copy()
     clean_df = (df
             .pipe(groupby_age)
             .pipe(filter_columns, relevant_cols = relevant_cols)
@@ -89,3 +90,71 @@ def early_onset(df, early_age = 50):
     return df
 
 # ==================================
+# ==================================
+# ==================================
+# ==================================
+# ==================================
+
+def clean_sigs(raw_df):
+    '''
+    Collects all data cleaning functions for signnature data into one pipeline,
+    returning the clean data.
+    '''
+
+    df = raw_df.copy()
+
+    clean_df = (df 
+            .pipe(add_normal_colon)
+            .pipe(sbs_strip)
+            .pipe(basis_change_first)
+            .pipe(add_mutation)
+            )
+
+    return clean_df
+
+# # ================================
+
+def sbs_strip(df):
+    ''' 
+    Only retain the SBS index (remove GRCh38, for example).
+    '''
+
+    df.columns = df.columns.str.split('_').str[0]
+    return df
+
+# # ================================
+# The following are for the signature data.
+# From Liang?
+
+def add_normal_colon(df):
+    ''' 
+    Add a normal colon for reference, weighted
+        0.4*SBS1 + 0.4*SBS5 + 0.2*SBS18 .
+    '''
+
+    df["Normal_colon"] = 0.4*df["SBS1_GRCh38"] + 0.4*df["SBS5_GRCh38"] + 0.2*df["SBS18_GRCh38"]
+    return df 
+
+# ==================================
+
+def basis_change_first(df):
+    ''' 
+    Reorder the signatures to be sorted by basis change first
+    (and thus match that on the web).
+    '''
+
+    # Map takes "A[C>A]A --> [C>A]AA", e.g.
+    df["context_ordered"] = df["Context"].map(lambda a: a[1:-1] + a[0] + a[-1])
+    sorted = df.sort_values(by = ["context_ordered"]).reset_index(drop = True)
+    sorted = sorted.drop("context_ordered", axis = 1)
+
+    return sorted
+
+# ==================================
+
+def add_mutation(df):
+    ''' 
+    Adds the specific mutation (e.g. A>C).
+    '''
+    df["Mutation"] = df["Context"].map(lambda a: a[2:-2])
+    return df
